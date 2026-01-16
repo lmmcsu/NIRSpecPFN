2187f5b55ab203c61de96f8477683d367ffaea3f
LiMiaomiao <242311023@csu.edu.cn>
2026-01-16 11:51:14 +0800

    Update import statements and add project root path
    Added project root directory to Python path for module imports.

--- wheat/wheat NIRSpecPFN.py
+++ wheat/wheat NIRSpecPFN.py
@@ -2,15 +2,19 @@ import numpy as np
 import pandas as pd
 import torch
 import os
+import sys
 from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score
 from sklearn.model_selection import KFold, train_test_split
 import matplotlib.pyplot as plt
 from tabpfn import TabPFNRegressor, TabPFNClassifier
 
+# Add project root directory to Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))  # current directory
+project_root = os.path.dirname(current_dir)               # relative directory
+sys.path.insert(0, project_root)                         # Add to search path
+
 # Preprocessing functions
-from preprocessing.process import (
-    msc, snv, derivative, savitzky_golay, airPLS
-)
+from preprocessing.process import (msc, snv, derivative, savitzky_golay, airPLS)
 
 # ==============================================================
 # Load TabPFN model
@@ -243,3 +247,4 @@ plt.title(f"True vs Predicted (Best preprocessing = {last_run_best_method})")
 plt.legend()
 plt.grid(True)
 plt.show()
+
